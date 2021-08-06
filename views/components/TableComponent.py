270ea335762d5b85077f6ff6b58de06fa3c44d81
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2021-08-06 20:54:54 -0300

    fix: TableComponent

--- views/components/TableComponent.py
+++ views/components/TableComponent.py
@@ -10,14 +10,14 @@ class TableComponent(Component):
 
     def __init__(self, headers: List = None, values: List = None, **kwargs):
         super().__init__(**kwargs)
-        self.headers = headers
-        self.values = values
+        self.headers = headers if headers is not None else []
+        self.values = values if values is not None else []
 
     def render(self):
         text = '| ' + ' | '.join(self.headers) + " |\n" \
                + '| ' + ' | '.join(['---' for _ in range(len(self.headers))]) + " |\n"
 
-        for value in self.values:
-            text += '| ' + ' | '.join(value) + " |\n"
+        for row in self.values:
+            text += '| ' + ' | '.join([str(value) for value in row]) + " |\n"
 
         self.render_component.markdown(text)
