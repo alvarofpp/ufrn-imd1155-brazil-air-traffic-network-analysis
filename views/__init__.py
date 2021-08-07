90120c031ba61d882c137374173553576a4effad
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2021-08-07 19:00:47 -0300

    chore: removes FoliumView

--- views/__init__.py
+++ views/__init__.py
@@ -1,6 +1,5 @@
 from .AirportsView import AirportsView
 from .CoreDecomposition import CoreDecomposition
-# from .FoliumView import FoliumView
 from .GraphView import GraphView
 from .IntroView import IntroView
 from .NodeRanking import NodeRanking
