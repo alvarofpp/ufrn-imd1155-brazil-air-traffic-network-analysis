6265946e70d24b17d422e95f348e0a3805783b2b
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2021-08-07 16:41:05 -0300

    feat: NodeRanking

--- views/NodeRanking.py
+++ views/NodeRanking.py
@@ -1,13 +1,8 @@
-import pandas as pd
-import networkx as nx
-from typing import Dict
 from .View import View
-from views.components import TableComponent
-from bokeh.io import output_file, show
-from bokeh.models import (BoxZoomTool, Circle, HoverTool,
-                          MultiLine, Plot, Range1d, ResetTool, PanTool, ZoomInTool, ZoomOutTool)
-from bokeh.palettes import Spectral4
-from bokeh.plotting import from_networkx
+from views.components import TableComponent, PanelTabsBokehComponent
+from utils.constants import METRICS
+from views.charts import DegreeCentralityChart, ClosenessCentralityChart, \
+    BetweennessCentralityChart, EigenVectorCentralityChart
 
 
 class NodeRanking(View):
@@ -15,44 +10,27 @@ class NodeRanking(View):
         self.render_component.markdown("""
         ## Node Ranking 
         """)
-        cols = self.render_component.beta_columns([7, 3])
-        #
-        # # Column 1
+        cols = self.render_component.beta_columns(self._columns_width)
+
+        # Column 1
         graph_selected = cols[1].selectbox('Select the graph', options=list(graphs.keys()), key='node_ranking')
         graph = graphs[graph_selected]
 
+        TableComponent(
+            render_component=cols[1],
+            headers=TableComponent.metric_headers,
+            values=[
+                METRICS['degree_centrality'],
+                METRICS['betweenness_centrality'],
+                METRICS['closeness_centrality'],
+                METRICS['eigen_vector_centrality'],
+            ]
+        ).render()
+
         # Column 0
-
-        # Prepare Data
-        G = graph
-
-        SAME_CLUB_COLOR, DIFFERENT_CLUB_COLOR = "black", "red"
-        edge_attrs = {}
-
-        for start_node, end_node, _ in G.edges(data=True):
-            edge_color = SAME_CLUB_COLOR if G.nodes[start_node]["country"] == G.nodes[end_node][
-                "country"] else DIFFERENT_CLUB_COLOR
-            edge_attrs[(start_node, end_node)] = edge_color
-
-        nx.set_edge_attributes(G, edge_attrs, "edge_color")
-
-        # Show with Bokeh
-        plot = Plot(plot_width=400, plot_height=400,
-                    x_range=Range1d(-1.1, 1.1), y_range=Range1d(-1.1, 1.1))
-        plot.title.text = "Graph Interaction Demonstration"
-
-        node_hover_tool = HoverTool(tooltips=[("index", "@index"), ("country", "@country")])
-        plot.add_tools(node_hover_tool, BoxZoomTool(), ResetTool(), PanTool(), ZoomInTool(), ZoomOutTool())
-
-        graph_renderer = from_networkx(G, nx.spring_layout, scale=1, center=(0, 0))
-
-        graph_renderer.node_renderer.glyph = Circle(size=15, fill_color=Spectral4[0])
-        graph_renderer.edge_renderer.glyph = MultiLine(line_color="edge_color", line_alpha=0.8, line_width=1)
-        plot.renderers.append(graph_renderer)
-
-        cols[0].bokeh_chart(plot)
-    # TODO Degree Centrality
-    # TODO Closeness Centrality
-    # TODO Betweenness Centrality
-    # TODO EigenVector Centrality
-    # TODO All together
+        PanelTabsBokehComponent(render_component=cols[0]) \
+            .add_tab('Degree Centrality', DegreeCentralityChart().get(graph)) \
+            .add_tab('Closeness Centrality', ClosenessCentralityChart().get(graph)) \
+            .add_tab('Betweenness Centrality', BetweennessCentralityChart().get(graph)) \
+            .add_tab('Eigen Vector Centrality', EigenVectorCentralityChart().get(graph)) \
+            .render()
