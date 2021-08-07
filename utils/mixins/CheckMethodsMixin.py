4fbccd3be24bdc9a42bd47326feb6e06592fd544
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2021-08-07 12:26:50 -0300

    fix: replaces NotImplemented to NotImplementedError

--- utils/mixins/CheckMethodsMixin.py
+++ utils/mixins/CheckMethodsMixin.py
@@ -5,4 +5,4 @@ class CheckMethodsMixin:
         methods = [method for method in dir(self) if method in self._methods]
         if len(methods) != len(self._methods):
             diff_methods = set(self._methods).difference(methods)
-            raise NotImplemented('You must implement the methods: {}.'.format(', '.join(diff_methods)))
+            raise NotImplementedError('You must implement the methods: {}.'.format(', '.join(diff_methods)))
