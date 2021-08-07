0e22dae69fb1d0b3cf68ae69e187cfe723d2fbc9
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2021-08-07 16:36:43 -0300

    chore: TableComponent can render or return markdown

--- views/components/TableComponent.py
+++ views/components/TableComponent.py
@@ -1,5 +1,6 @@
 from typing import List
 from .Component import Component
+from utils.constants import MODE_RENDER, MODE_MARKDOWN
 
 
 class TableComponent(Component):
@@ -13,11 +14,22 @@ class TableComponent(Component):
         self.headers = headers if headers is not None else []
         self.values = values if values is not None else []
 
-    def render(self):
+    def render(self, mode: int = MODE_RENDER):
         text = '| ' + ' | '.join(self.headers) + " |\n" \
                + '| ' + ' | '.join(['---' for _ in range(len(self.headers))]) + " |\n"
 
         for row in self.values:
             text += '| ' + ' | '.join([str(value) for value in row]) + " |\n"
 
+        if mode not in [MODE_RENDER, MODE_MARKDOWN]:
+            raise ValueError(
+                'Invalid mode. Expect [{}], given {}'.format(', '.join([str(MODE_RENDER), str(MODE_MARKDOWN)]), str(mode))
+            )
+
+        if mode == MODE_MARKDOWN:
+            return text
+
         self.render_component.markdown(text)
+
+    def get_markdown(self) -> str:
+        return self.render(MODE_MARKDOWN)
