3e6c0638ba28f3b07b7f774c3dc4150393e62a7b
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2021-08-08 14:50:32 -0300

    perf: get_data

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import copy
 from typing import List, Dict
 import networkx as nx
 import streamlit as st
@@ -6,13 +7,17 @@ from views.components import SelectComponent
 from utils.constants import *
 
 
-@st.cache(allow_output_mutation=True)
+@st.cache()
 def get_data() -> Dict:
-    return {
-        '2019': nx.read_graphml('data/air_traffic_2019.graphml'),
-        '2020': nx.read_graphml('data/air_traffic_2020.graphml'),
-        '2021': nx.read_graphml('data/air_traffic_2021.graphml'),
-    }
+    years = [2019, 2020, 2021]
+    data = {}
+
+    for year in years:
+        with open('data/air_traffic_{}.graphml'.format(year), mode='r') as file:
+            content = file.read()
+            data[str(year)] = nx.parse_graphml(content)
+
+    return data
 
 
 @st.cache()
@@ -35,7 +40,7 @@ def mode_my_graph():
     graph = None
     stub_content = get_stub_graphml()
     file_uploaded = st.sidebar.file_uploader('Import your graph', type='graphml')
-    graphml_expander = st.sidebar.beta_expander('Visualizar modelo de grafo')
+    graphml_expander = st.sidebar.expander('View graph file template')
     graphml_expander.text(stub_content)
 
     if file_uploaded:
@@ -68,11 +73,14 @@ def main():
         page_title='Brazil air traffic network analysis',
         layout='wide'
     )
+    data = copy.deepcopy(get_data())
     st.title('Brazil air traffic network analysis')
-    IntroView(render_component=st.sidebar).render()
-    data = get_data()
+    IntroView().render()
 
-    st.sidebar.markdown('You can use a existent graph or import your graph to app.')
+    st.sidebar.markdown("""
+    # Select the graph that you want to analyze
+    You can use a existent graph or import your graph to app.
+    """)
     data_mode_selected = SelectComponent({
         BASE_APP: 'Use existent graphs in the app',
         BASE_MY_GRAPH: 'Import my graph',
